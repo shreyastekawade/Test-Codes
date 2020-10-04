2a4289d6b313c50ae437f9c71e88348fb8607b8f
Shreyas Tekawade <71271577+shreyastekawade@users.noreply.github.com>
2020-10-04 20:37:13 +0530

    Add files via upload

---
+++ upload.java
@@ -0,0 +1,53 @@
+package com.traing.servlets;
+
+import java.io.FileReader;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.training.entity.Employee;
+
+/**
+ * Servlet implementation class upload
+ */
+@WebServlet("/upload")
+public class upload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public upload() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	 String str = request.getParameter("filename");
+	 
+	 System.out.println(str);
+	 Gson gson = new Gson();
+		//Employee obj = new Employee();
+		Employee [] obj = gson.fromJson(new FileReader(str), Employee[].class);
+		for (Employee employee : obj) {
+			System.out.println(employee);
+	}
+
+}
+}
